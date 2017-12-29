7c3e546b65ed11818e111329d8074e9aa4c53154
aglutz <34938022+aglutz@users.noreply.github.com>
2017-12-29 20:24:04 +0100

    first commit

---
+++ GlutzModTest1/module.php
@@ -0,0 +1,48 @@
+<?
+    // Klassendefinition
+    class GlutzModTest1 extends IPSModule {
+
+        // Der Konstruktor des Moduls
+        // Überschreibt den Standard Kontruktor von IPS
+        public function __construct($InstanceID) {
+            // Diese Zeile nicht löschen
+            parent::__construct($InstanceID);
+
+            // Selbsterstellter Code
+        }
+
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+
+            $this->RegisterTimer("UpdateTimer", 10 * 1000, 'GLUTZ_UpdateInstance($_IPS[\'TARGET\']);');
+        }
+
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+        }
+
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * ABC_MeineErsteEigeneFunktion($id);
+        *
+        */
+        public function MeineErsteEigeneFunktion(int $id) {
+            // Selbsterstellter Code
+            echo $id;
+        }
+
+        public function UpdateInstance() {
+            // Selbsterstellter Code
+            echo "Hello Update Event.....";
+        }
+
+
+
+    }
+?>
